79d2ce978c8aef971da783f05153955616e8decd
luscadigital <joey@luscadigital.com>
2015-02-11 20:25:35 -0500

    only show those that are ready

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -167,7 +167,7 @@ class DataController extends Controller {
 
   public function showTickers()
   {
-    return view('index')->withTickers(Ticker::paginate(10));
+    return view('index')->withTickers(Ticker::where('status', 1)->paginate(10));
   }
 
   public function spin()
