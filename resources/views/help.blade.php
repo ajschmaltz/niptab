16ce2834cc78a4836652038845e3d238e250864b
luscadigital <joey@luscadigital.com>
2015-02-27 03:45:37 -0500

    spell check

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -11,7 +11,7 @@
       <p>The filing types it saves is completely up to you.</p>
       <h3>Setting Patterns</h3>
       <p>Patterns are specific to certain <a href="/types">types</a>.  You have to be looking for at least one type of filing before you can specify a pattern to look for.</p>
-      <p>The tool is constantly running through the filings and going through them looking for matches to <a href="/patterns">patthers</a>.</p>
+      <p>The tool is constantly running through the filings and going through them looking for matches to <a href="/patterns">patterns</a>.</p>
       <p>The patterns are a regular expression with one matching group.  The tool will save the matching group as the value and the entire match as the source.</p>
       <h3>Uploading Tickers</h3>
       <p>Tickers are uploaded from CSV files.  The file needs to have one column named "Symbol" with the stock symbol.  It can have additional columns but they wil not be saved.</p>
