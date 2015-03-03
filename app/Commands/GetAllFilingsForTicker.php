7a2a01e4d1757d77217998b421f8a1e8f30fd544
luscadigital <joey@luscadigital.com>
2015-03-03 14:43:49 -0500

    leave

--- app/Commands/GetAllFilingsForTicker.php
+++ app/Commands/GetAllFilingsForTicker.php
@@ -61,8 +61,6 @@ class GetAllFilingsForTicker extends Command implements SelfHandling {
       return null;
     }
 
-    $ticker->filings()->where('type_id', $type->id)->delete();
-
     $ticker->filings()->create([
       'type_id' => $type->id,
       'link' => $this->getLinkToFiling($filings, $type->name),
