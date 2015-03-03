863d78a8cf230d8ceefbfb20d0cf053e9aba44c3
ajschmaltz <drew@mainstreetmower.com>
2015-03-03 14:18:26 -0500

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,7 +32,8 @@
           @forelse($data as $datum)
             <tr>
               <td>{{ $datum->id }}</td>
-              <td>{{ $datum->filing }}</td>
+              <td>{{ $datum->filing->ticker->symbol }}</td>
+              <td><a href="{{ $datum->filing->link }}">{{ $datum->filing->type->name }}</a></td>
               <td>{{ $datum->updated_at->format("F j, Y, g:i a") }}</td>
               <td>{{ $datum->pattern->finds }}</td>
               <th>{{ $datum->value }}</th>
